4329dcb3930c5cfb2d35ae8a4f78bd68c46a6ecc
Luka Kereceni <luka.kereceni@gmail.com>
2021-08-03 10:13:06 +0200

    Added babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log({ leatherStrips: 2, ironIngot: 1, refinedMoonstone: 4 });
+const object1 = {
+    leatherStrips: 2,
+    ironIngot: 1,
+    refinedMoonstone: 4,
+}
+
+const object2 = {
+    ...object1,
+    leather: 1,
+    refinedMoonstone: 3,
+}
+
+console.log(object1);
+console.log(object2);
